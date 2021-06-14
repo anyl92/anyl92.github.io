86d31402aa43c1fa3d19276367f6add06f349e2c
anyl92 <yulim9592@gmail.com>
2021-06-14 23:43:23 +0900

    Add basic file

---
+++ js/result.js
@@ -0,0 +1,105 @@
+// import {KAKAO_KEY} from '../config';
+// console.log(KAKAO_KEY);
+
+var kakaokey = env.KAKAO_KEY;
+Kakao.init(kakaokey);
+
+const typeSet = [
+  {
+    text: "센치한 도도형 고양이",
+    description: "친절한 편이지만 만만한 성격은 아닌 유형입니다. 평소에는 밝고 친절하나, 와인을 마시면 복잡한 내면을 드러내는 편이에요. 조용히 홀짝홀짝 마시는 스타일이죠. 시끄러운 분위기보다는 차분하고 밀도 높은 대화를 선호하기 때문에 여러 명보다는 한두 명 모이는 자리를 선호할 것 같아요.",
+    textWine: "소비뇽 블랑",
+    textType: "고독한 배려형",
+    img: "img/type_cat.png",
+    imgWine: "img/wine_white.png",
+    imgType: "img/match_sloth.png"
+  },
+  {
+    text: "고독한 배려형 나무늘보",
+    description: "남들의 이야기에 귀를 기울여주는 배려심이 높은 유형입니다. 무난하게 주변 사람들과 잘 어울리며 친구들이 고민이 생겼을 때 찾는 1순위 친구인 것 같아요. 포커페이스에 약하고 공감 능력이 뛰어나 상대방으로부터 위로감을 느낄 수 있게 도와주네요.",
+    textWine: "소비뇽 블랑",
+    textType: "센치한 도도형",
+    img: "img/type_sloth.png",
+    imgWine: "img/wine_red.png",
+    imgType: "img/match_cat.png"
+  },
+  {
+    text: "멀뚱멀뚱 무적형 토끼",
+    description: "화기애애한 성격과 긍정적인 마인드를 갖고 있는 유형입니다. 남들이 취할 때도 멀쩡히 자신만의 노하우로 안 취해본 경험이 있지 않나요? 대체로는 분위기에 무난하게 적응하는 타입이에요. 사람들과 어울리는 것을 싫어하지는 않지만 가끔씩은 나만의 시간을 갖는 것도 중요하게 생각하는군요.",
+    textWine: "카베르네 소비뇽",
+    textType: "다다익선 햄스터형",
+    img: "img/type_rabbit.png",
+    imgWine: "img/wine_red.png",
+    imgType: "img/match_hamster.png"
+  },
+  {
+    text: "도전적 다다익선형 햄스터",
+    description: "도전적이며 다양성을 추구하는 유형입니다. 쉽게 포기하지 않고 이것저것 시도해보는 도전정신은 와인의 세계에서도 발휘될 수 있겠네요. 남들보다 풍부한 경험을 갖고 있어, 지혜로운 조언이나 대화를 이끌어내기도 하겠어요. 여러 가지 매력을 갖고 있는 다다익선일 확률이 높아요.",
+    textWine: "카베르네 소비뇽",
+    textType: "멀뚱멀뚱 무적형",
+    img: "img/type_hamster.png",
+    imgWine: "img/wine_red.png",
+    imgType: "img/match_rabbit.png"
+  },
+  {
+    text: "모여모여 친목형 펭귄",
+    description: "혼자 마시는 것보다 여러 명이 함께 마실 때 더 맛있다고 느끼는 당신은 화끈하지만 진득한 유형입니다. 눈치가 빠르고 스마트해서 사람들과 커뮤니케이션할 때도 큰 어려움이 없을 것 같네요. 분위기에 적응하기보다는, 대화를 이끌고 좋은 분위기를 만들어낼 것 같아요.",
+    textWine: "소비뇽 블랑",
+    textType: "수다쟁이 댕댕이형",
+    img: "img/type_penguin.png",
+    imgWine: "img/wine_red.png",
+    imgType: "img/match_dog.png"
+  },
+  {
+    text: "수다쟁이 댕댕이형 강아지",
+    description: "밝고 명랑한 성격을 가지고 있어 여러 사람들에게 사랑과 관심을 받는 유형입니다. 유쾌하고 재미있는 아이디어로 대화를 즐겁게 만들어주는 능력을 갖고 있네요. 파티를 좋아하는 당신은 와인 또한 특별하고 화려한 와인을 좋아할 확률이 높아요.",
+    textWine: "소비뇽 블랑",
+    textType: "모여모여 친목형",
+    img: "img/type_dog.png",
+    imgWine: "img/wine_red.png",
+    imgType: "img/match_penguin.png"
+  }
+]
+
+const loadResult = () => {
+  const type = typeSet[localStorage.wineType];
+
+  document.querySelector(".img-type").src = type.img;
+  document.querySelector(".type-text").innerHTML = type.text;
+  document.querySelector(".type-description").innerHTML = type.description;
+
+  document.querySelector(".img-match.wine").src = type.imgWine;
+  document.querySelector(".match-sub-text.wine").innerHTML = type.textWine;
+  document.querySelector(".img-match.type").src = type.imgType;
+  document.querySelector(".match-sub-text.type").innerHTML = type.textType;
+}
+
+window.addEventListener("load", () => {
+  loadResult();
+})
+
+const modal = document.querySelector(".modal");
+let modalState = false;
+
+window.addEventListener("click", function (event) {
+  clickElement = event.target.className;
+
+  if (modalState) {
+    event.preventDefault();
+
+    if (clickElement == "modal-yes-button") {
+      window.location.href = "/survey.html";
+      localStorage.removeItem("wineType");
+    }
+    if (event.target.dataset.name != "modal") {
+      modal.style.display = "none";
+      modalState = false;
+    }
+  } else if (clickElement == "restart-button") {
+    modal.style.display = "block";
+    modalState = true;
+
+    modal.style.top = `${window.innerHeight/2 - modal.offsetHeight/2 + window.scrollY}px`;
+    modal.style.left = `${window.innerWidth/2 - modal.offsetWidth/2 + window.scrollX}px`;
+  }
+})
---
+++ js/survey.js
@@ -0,0 +1,147 @@
+
+const questionSet = [
+  {
+    question: "와인 매장에 오니, 궁금한 와인을\n찾았어요. 이때 내 행동은?",
+    answerA: "점원한테 직접 물어볼거예요.",
+    answerB: "어플을 켜서 와인명을 검색해 볼 거예요."
+  },
+  {
+    question: "마음에 드는 와인을 샀어요.\n다음 목적지는 어디일까요?",
+    answerA: "화려한 파티로 이동해요.",
+    answerB: "가까운 친구 집에 놀러 가요."
+  },
+  {
+    question: "파티에 도착했더니 모르는 사람들이\n많아요. 이때 내 반응은?",
+    answerA: "먼저 가서 인사하고 이야기해요.",
+    answerB: "아는 사람들이 있는 곳을 찾아요."
+  },
+  {
+    question: "둘러보니 안주랑 와인이 많이\n준비되어 있네요. 이때 내 행동은?",
+    answerA: "모르는 와인, 안주에 도전해 볼 거예요.",
+    answerB: "더 이상 안 마시고 이야기만 할 거예요."
+  },
+  {
+    question: "벌써 와인 한 병을 다 마셨네요!\n이때 내 상태는?",
+    answerA: "헤롱헤롱 기분이 좋은 상태로 집에 가요.",
+    answerB: "멀쩡한 상태로 한 병을 더 마실까 고민해요."
+  },
+  {
+    question: "친구 집에 도착했어요.\n이때 가장 먼저 하는 행동은?",
+    answerA: "가볍게 와인부터 마셔요.",
+    answerB: "같이 먹을 음식부터 준비해요."
+  },
+  {
+    question: "벌써 와인 한 병을 다 마셨네요!\n이때 내 상태는?",
+    answerA: "헤롱헤롱 기분이 좋은 상태로 집에 가요.",
+    answerB: "멀쩡한 상태로 한 병을 더 마실까 고민해요."
+  },
+  {
+    question: "취한 친구가 아까부터 똑같은 이야기를\n반복하고 있어요. 이때 내 반응은?",
+    answerA: "'술 좀 깨게 찬물 좀 마셔'라고 말해요.",
+    answerB: "'그랬구나.. 응응' 하고 공감해 줘요."
+  }
+]
+
+let $containerTag = document.querySelector(".survey");
+const loadQuestion = () => {
+  for (var i=0; i < 6; i++) {
+    $containerTag.children[i].style.width = $containerTag.offsetWidth + "px";
+  }
+
+  $containerTag.style.width = ($containerTag.offsetWidth * 6) + "px";
+  $containerTag.style.left = "0px";
+  $containerTag.style.fontSize = 0;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadQuestion();
+})
+
+const slideQuestion = () => {
+  $containerTag.style.transition = "0.5s ease-out";
+  $containerTag.style.left = (parseInt($containerTag.style.left) 
+            - ($containerTag.offsetWidth / 6)) + "px";
+}
+
+const paintButton = (button) => {
+  button.style.backgroundColor = "#000000";
+  button.style.color = "#FFFFFF";
+}
+
+let typeNum = 0;
+const answers = [];
+const buttons = document.querySelectorAll(".survey-answer");
+
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    if (modalState) {return}
+
+    paintButton(button);
+    answers.push(button.classList[1]);
+
+    if (answers.length === 3) {
+      if (answers[2] === "B") {
+        typeNum = 3;
+      }
+    }
+
+    if (answers.length === 6) {
+      if (typeNum) {
+        if (answers[4] === "B") {
+          localStorage.wineType = 3
+        } else if (answers[5] === "A") {
+          localStorage.wineType = 1
+        } else if (answers[5] === "B") {
+          localStorage.wineType = 2
+        }
+      } else {
+        if (answers[3] === "A") {
+          localStorage.wineType = 5
+        } else if (answers[4] === "A") {
+          localStorage.wineType = 4
+        } else if (answers[5] === "A") {
+          localStorage.wineType = 6
+        }
+      }
+      window.location.href = "/result.html"
+
+      return
+    }
+    
+    nextEl = button.parentElement.nextElementSibling;
+    nextNum = nextEl.dataset.id - 2 + typeNum;
+    nextText = questionSet[nextNum];
+    
+    nextEl.children[1].innerText = nextText.question;
+    nextEl.children[2].innerText = nextText.answerA;
+    nextEl.children[3].innerText = nextText.answerB;
+
+    setTimeout(function () {
+      slideQuestion();
+    }, 120);
+  })
+})
+
+
+const modal = document.querySelector(".modal");
+let modalState = false;
+
+window.addEventListener("click", function (event) {
+  clickElement = event.target.className;
+
+  if (modalState) {
+    if (clickElement == "modal_yes_button") {
+      window.location.href = "/index.html";
+    }
+    if (event.target.dataset.name != "modal") {
+      modal.style.display = "none";
+      modalState = false;
+    }
+  } else if (clickElement == "img-back-arrow") {
+    modal.style.display = "block";
+    modalState = true;
+
+    modal.style.top = `${window.innerHeight/2 - modal.offsetHeight/2 + window.scrollY}px`;
+    modal.style.left = `${window.innerWidth/2 - modal.offsetWidth/2 + window.scrollX}px`;
+  }
+})
